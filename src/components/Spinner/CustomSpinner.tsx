9cfaff4f93f1d6e93caed2ffaa247b457e0410b0
abdallah608 <abdallahhassanshaaban@gmail.com>
2024-03-30 03:06:38 +0200

    first commit

--- src/components/Spinner/CustomSpinner.tsx
+++ src/components/Spinner/CustomSpinner.tsx
@@ -3,8 +3,8 @@ const CustomSpinner = () => {
   return (
     <div className="flex justify-center items-center">
       <span className="relative h-20 w-20 ">
-        <span className="absolute bg-green-700 w-full h-full rounded-full animate-ping blur-md"></span>
-        <span className="relative h-20 w-20 bg-green-600 blur-xl  rounded-full inline-flex "></span>
+        <span className="absolute bg-sky-700 w-full h-full rounded-full animate-ping blur-md"></span>
+        <span className="relative h-20 w-20 bg-sky-600 blur-xl  rounded-full inline-flex "></span>
       </span>
     </div>
   );
